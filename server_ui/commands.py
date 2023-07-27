affe4f0b8376c3712a8fcd80e1b5e930c25e3184
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2023-07-27 13:07:52 +0300

    removed server node_modules

--- server_ui/commands.py
+++ server_ui/commands.py
@@ -40,7 +40,37 @@ def stop_js_server():
         finally:
             server_process = None
 
-# Test the functions
-#start_js_server()
-#time.sleep(100)  # Simulating server running for 10 seconds
-#stop_js_server()
+def start_frontend_server():
+    global server_process
+    if server_process is None:
+        try:
+            # Change the working directory to your server directory where the package.json is located.
+            # Use a raw string (prefix 'r') to avoid special characters causing issues.
+            os.chdir(r'X:\programming\JS\allin\client\react-redux-tsx')
+
+            # Start the JavaScript server using 'npm start'.
+            server_process = subprocess.Popen(['npm', 'start'], shell=True)
+
+            print("JavaScript server started.")
+        except Exception as e:
+            print("Error starting the JavaScript server:", e)
+
+def stop_backend_server():
+    global server_process
+    if server_process is not None:
+        try:
+            # Send a SIGINT signal to gracefully stop the server.
+            server_process.send_signal(signal.CTRL_C_EVENT)
+
+            # Wait for the server process to finish.
+            server_process.wait(timeout=10)
+
+            print("JavaScript server stopped.")
+        except subprocess.TimeoutExpired:
+            # If the server process doesn't stop gracefully, force termination.
+            server_process.terminate()
+            print("JavaScript server forcefully terminated.")
+        except Exception as e:
+            print("Error stopping the JavaScript server:", e)
+        finally:
+            server_process = None
--- server_ui/server.py
+++ server_ui/server.py
@@ -1,11 +1,81 @@
+import sys
 import commands as commands
-import tkinter as ui 
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QTextEdit
+from PySide6.QtCore import QTextStream, Qt
+from PySide6.QtGui import QTextCursor
 
-root = ui.Tk()
-root.geometry("1024x576")
-root.configure(background='black')
-frm = ui.Frame(root)
-frm.grid()
-ui.Button(frm, text='Start server', command=commands.start_js_server, background='aqua').grid(column=1, row=0)
-ui.Button(frm, text='Stop server', command=commands.stop_js_server, background='red').grid(column=2, row=0)
-root.mainloop()
+class ConsoleRedirector:
+    def __init__(self, text_edit):
+        self.text_edit = text_edit
+
+    def write(self, text):
+        cursor = self.text_edit.textCursor()
+        cursor.movePosition(QTextCursor.End)
+        cursor.insertText(text)
+        self.text_edit.setTextCursor(cursor)
+        self.text_edit.ensureCursorVisible()
+
+class TestApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle("PySide6 Test")
+        self.setGeometry(100, 100, 1024, 720)
+        self.setStyleSheet("""
+            background-color: #404040;
+            color: white;
+        """)
+
+        # Widgets
+        self.label = QLabel("Server status: OFF", self)
+        self.label.move(50, 50)
+
+        self.button_start = QPushButton("Start server", self)
+        self.button_start.clicked.connect(self.on_button_click_start)
+        self.button_start.setFixedSize(200, 30)
+        self.button_start.setStyleSheet("""
+            background-color: #00FF00;
+            color: black;
+            border: 2px solid #1c7430;
+            border-radius: 10px;
+            padding: 5px 10px;
+        """)
+        self.button_start.move(50, 100)
+
+        self.button_stop = QPushButton("Stop server", self)
+        self.button_stop.clicked.connect(self.on_button_click_stop)
+        self.button_stop.setFixedSize(200, 30)
+        self.button_stop.setStyleSheet("""
+            background-color: #FF0000;
+            color: black;
+            border: 1px solid #BF6565;
+            border-radius: 10px;
+            padding: 5px 10px;
+        """)
+        self.button_stop.move(50, 150)
+
+        self.console_output = QTextEdit(self)
+        self.console_output.setGeometry(300, 50, 700, 400)
+        self.console_output.setStyleSheet("""
+            background-color: #303030;
+            color: white;
+        """)
+
+        # Redirect console output to the QTextEdit widget
+        sys.stdout = ConsoleRedirector(self.console_output)
+
+    def on_button_click_start(self):
+        self.label.setText("Server status: ON")
+        commands.start_js_server()
+
+    def on_button_click_stop(self):
+        self.label.setText("Server status: OFF")
+        commands.stop_js_server()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = TestApp()
+    window.show()
+    sys.exit(app.exec())
