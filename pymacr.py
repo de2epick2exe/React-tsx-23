5dfdc25b7131960dadb3d1a89c85e5cf67c7b2ea
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2023-07-27 17:56:14 +0300

    delete py file

--- pymacr.py
+++
@@ -1,9 +0,0 @@
-import pyautogui    #these are for our clicks and keyboard strokes!
-import time         #required to call any time commands (i.e. delays)
-
-time.sleep(1)
-print(pyautogui.position())
-
-pyautogui.click(2352, 205)
-while True:
-    pyautogui.keyDown(']')
