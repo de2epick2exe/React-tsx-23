58aa18cdde64381629c4b25238cbb288533008e7
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2023-08-15 21:59:29 +0300

    Login reUI

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,45 +1,82 @@
-import { Button, Input } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
-import { loginUser, setToken } from '../store/reduses/UserSlice';
-import { useSelector, useDispatch } from 'react-redux';
-import { ThunkDispatch } from '@reduxjs/toolkit';
-import { RootState } from '../store/store';
+import { Button, Input } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { loginUser, setToken } from "../store/reduses/UserSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { ThunkDispatch } from "@reduxjs/toolkit";
+import { RootState } from "../store/store";
 import { Link, useNavigate } from "react-router-dom"; // Import useHistory hook
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  Stack,
+  StackDivider,
+} from "@chakra-ui/react";
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+
 const Login = () => {
-  const [usern_or_em, setUsern_or_Em] = useState('');
-  const [password, setPassword] = useState('');
+  const [usern_or_em, setUsern_or_Em] = useState("");
+  const [password, setPassword] = useState("");
   const token = useSelector((state: RootState) => state.userReducer.token);
   const dispatch: ThunkDispatch<any, any, any> = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const login = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    
+
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (emailRegex.test(usern_or_em)) {
-      console.log('Valid email');
-      await dispatch(loginUser('', usern_or_em, password));
+      console.log("Valid email");
+      await dispatch(loginUser("", usern_or_em, password));
     } else {
-      console.log('Valid username');
-      await dispatch(loginUser(usern_or_em, '', password));
+      console.log("Valid username");
+      await dispatch(loginUser(usern_or_em, "", password));
     }
-    navigate('/');
+    navigate("/");
   };
 
   return (
-    <div className="page">
-      <div>Login</div>
-      <div>Username or Email</div>
-      <Input value={usern_or_em} onChange={(e) => setUsern_or_Em(e.target.value)} />
-      <div>Password</div>
-      <Input value={password} onChange={(e) => setPassword(e.target.value)} />
-      <div>
-        <Button onClick={login}>Login</Button>
-      </div>
-      <div>
-        <Button as={Link} to="/register" >Register</Button>
-      </div>
-    </div>
+    <>
+      <Center marginTop={window.innerHeight / 7}>
+        <Card maxW="sm" backgroundColor={"red.800"}>
+          <CardHeader>
+            <Heading size="md">Login</Heading>
+          </CardHeader>
+          <CardBody>
+            <Stack divider={<StackDivider />} spacing="4">
+              <Box>
+                <Heading size="xs" textTransform="uppercase">
+                  Username or Email
+                </Heading>
+                <Input
+                  value={usern_or_em}
+                  onChange={(e) => setUsern_or_Em(e.target.value)}
+                />
+              </Box>
+
+              <Box>
+                <Heading size="xs" textTransform="uppercase">
+                  Password
+                </Heading>
+                <Input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Box>
+
+              <Box>
+                <Button onClick={login}>Login</Button>
+
+                <Button marginLeft={"10"} as={Link} to="/register">
+                  To Register
+                </Button>
+              </Box>
+            </Stack>
+          </CardBody>
+        </Card>
+      </Center>
+    </>
   );
 };
 
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,64 +1,81 @@
-import { Box, Button, Center, Flex, Heading, Input, Stack, StackDivider } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
-import { loginUser, register_user, setToken } from '../store/reduses/UserSlice';
-import { useSelector, useDispatch } from 'react-redux';
-import { ThunkDispatch } from '@reduxjs/toolkit';
-import { RootState } from '../store/store';
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Heading,
+  Input,
+  Stack,
+  StackDivider,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { loginUser, register_user, setToken } from "../store/reduses/UserSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { ThunkDispatch } from "@reduxjs/toolkit";
+import { RootState } from "../store/store";
 import { Link, useNavigate } from "react-router-dom"; // Import useHistory hook
-import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
 const Register = () => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const token = useSelector((state: RootState) => state.userReducer.token);
   const dispatch: ThunkDispatch<any, any, any> = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const register = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();  
-    
+    e.preventDefault();
+
     try {
-        await dispatch(register_user(username, email, password))
-        navigate('/');
+      await dispatch(register_user(username, email, password));
+      navigate("/");
     } catch (e) {
-        console.log(e)
-    } 
-     
+      console.log(e);
+    }
   };
 
   return (
     <Center marginTop={window.innerHeight / 7}>
-    <Card maxW='sm'  backgroundColor={'red.800'}>
-      <CardHeader  backgroundColor={'white'}><Heading size='md'>Login</Heading></CardHeader>
-      <CardBody>
-      <Stack divider={<StackDivider />} spacing='4'>
-      <Box>
-      <Heading size='xs' textTransform='uppercase'>
-      Username
-      </Heading>
-      <Input value={username} onChange={(e) => setUsername(e.target.value)} />
-      </Box>
-      <Box>
-      <Heading size='xs' textTransform='uppercase'>
-      Email
-      </Heading>
-      <Input value={email} onChange={(e) => setEmail(e.target.value)} />
-      
-      </Box>
-      <Box>
-      <Heading size='xs' textTransform='uppercase'>
-      Password
-      </Heading>
-      <Input value={password} onChange={(e) => setPassword(e.target.value)} />
-      
-      </Box>
+      <Card maxW="sm" backgroundColor={"red.800"}>
+        <CardHeader>
+          <Heading size="md">Register</Heading>
+        </CardHeader>
+        <CardBody>
+          <Stack divider={<StackDivider />} spacing="4">
+            <Box>
+              <Heading size="xs" textTransform="uppercase">
+                Username
+              </Heading>
+              <Input
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Box>
+            <Box>
+              <Heading size="xs" textTransform="uppercase">
+                Email
+              </Heading>
+              <Input value={email} onChange={(e) => setEmail(e.target.value)} />
+            </Box>
+            <Box>
+              <Heading size="xs" textTransform="uppercase">
+                Password
+              </Heading>
+              <Input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Box>
 
-      <Box>
-      <Button onClick={register}>Register</Button>
-      <Button marginLeft={'10'} as={Link} to="/login"> To Login</Button>
-      </Box>
-       </Stack>
-      </CardBody>
-    </Card>
+            <Box>
+              <Button onClick={register}>Register</Button>
+              <Button marginLeft={"10"} as={Link} to="/login">
+                {" "}
+                To Login
+              </Button>
+            </Box>
+          </Stack>
+        </CardBody>
+      </Card>
     </Center>
   );
 };
