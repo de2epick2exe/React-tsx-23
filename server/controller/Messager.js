4fb312735a8ccbf0ca74a08b0a1c0995b88a28e3
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2023-09-08 12:42:34 +0300

    add room route

--- server/controller/Messager.js
+++ server/controller/Messager.js
@@ -7,17 +7,71 @@ const Redis = require ('ioredis');
 
 class Messager{
 
-    async add_room(req, res){
+    async get_room(req, res){
         try{
-        const {roomid} = req.params
+        const {roomid, id} = req.params
         console.log(roomid)
-        res.json({mess:'room chek', roomid})
+        res.json({message:'room chek', roomid})
     }
         catch(err){
             console.log(err)
         }
     }
- 
+    
+
+    async create_private_room(req, res){
+        try {
+            const {id_1, id_2} = req.body
+            const room = await db.query("INSERT INTO rooms (type) VALUES ($1) RETURNING id",['PRIVATE'] )
+            const room_id = room.rows[0].id
+            
+            const room_u1 = await db.query("INSERT INTO conversations (user_id, room_id) VALUES ($1, $2)", [id_1, room_id])
+            const room_u2 = await db.query("INSERT INTO conversations (user_id, room_id) VALUES ($1, $2)", [id_2, room_id])
+            
+            const u1 = room_u1.rows[0]
+            const u2 = room_u2.rows[0]
+            
+            const data = {
+                status : 200,
+                room: "created"
+            }
+            res.json(data)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async users_messages_tab(req, res){
+        /*SELECT
+    c.id AS conversation_id,
+    r.type AS room_type,
+    m.content AS last_message_content,
+    m.date AS last_message_date
+FROM
+    public.conversations c
+RIGHT JOIN
+    public.rooms r
+ON
+    c.room_id = r.id
+LEFT JOIN (
+    SELECT
+        room_id,
+        MAX(date) AS max_date
+    FROM
+        public.messages
+    GROUP BY
+        room_id
+) AS latest_message
+ON
+    r.id = latest_message.room_id
+LEFT JOIN
+    public.messages m
+ON
+    latest_message.room_id = m.room_id
+    AND latest_message.max_date = m.date
+WHERE
+    c.user_id = 96; */
+    }
 
 }
 module.exports = new Messager()
--- server/routes/messager_routes.js
+++ server/routes/messager_routes.js
@@ -2,8 +2,8 @@ const Router = require('express')
 const Messager = require('../controller/Messager')
 const router = new Router()
 
-router.post('/:roomid', Messager.add_room)
-
+router.post('/g/:roomid', Messager.get_room)
+router.post('/new_private_room', Messager.create_private_room)
 
 
 module.exports = router
