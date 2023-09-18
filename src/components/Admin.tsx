6b289f06c6913f274b99c11abd4c970f99cb2453
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2023-09-18 12:52:26 +0300

    ban button ternary

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -2,7 +2,15 @@ import React, { useEffect, useState } from "react";
 import { RootState } from "../store/store";
 import { useSelector } from "react-redux";
 
-import { ban_user, get_all_users, get_last_online, get_last_registered, get_users_count, get_users_per_day, unBan_user } from "../unite/User_Functions";
+import {
+  ban_user,
+  get_all_users,
+  get_last_online,
+  get_last_registered,
+  get_users_count,
+  get_users_per_day,
+  unBan_user,
+} from "../unite/User_Functions";
 import { useNavigate } from "react-router-dom";
 import {
   Button,
@@ -27,6 +35,7 @@ import {
 } from "@chakra-ui/react";
 import { LuBan } from "react-icons/lu";
 import { IoIosSearch } from "react-icons/io";
+import { PiCaretDoubleUpBold } from "react-icons/pi";
 
 interface User {
   id: number;
@@ -40,65 +49,70 @@ const Admin = () => {
   const navigate = useNavigate();
   const data = useSelector((state: RootState) => state.userReducer);
   const [users, setUsers] = useState<User[]>([]);
-  const [total_users, setTotal_users] = useState<{ USERS_TOTAL: number[] }[]>([])
-  const [users_per_day, setUsers_per_day] = useState<{ USERS_PER_DAY: any[] }[]>([])  
+  const [total_users, setTotal_users] = useState<{ USERS_TOTAL: number[] }[]>(
+    []
+  );
+  const [users_per_day, setUsers_per_day] = useState<
+    { USERS_PER_DAY: any[] }[]
+  >([]);
   const data_role = "" || null;
- const [last_online, setLast_online] = useState<{ USERS_PER_DAY: any[] }[]>([])  
- const [last_registered, setLast_registered] = useState<{ USERS_PER_DAY: any[] }[]>([])  
+  const [last_online, setLast_online] = useState<{ USERS_PER_DAY: any[] }[]>(
+    []
+  );
+  const [last_registered, setLast_registered] = useState<
+    { USERS_PER_DAY: any[] }[]
+  >([]);
 
   const get_users = async () => {
     const response = await get_all_users(data.username, data.token);
     setUsers(response);
   };
-  const get_total_count =async () => {
+  const get_total_count = async () => {
     const response = await get_users_count(data.token);
-    console.log(response)
+    console.log(response);
     setTotal_users(response);
-    
-  }
-  const get_perday_count =async () => {
+  };
+  const get_perday_count = async () => {
     const response = await get_users_per_day(data.token);
-    console.log(response)
+    console.log(response);
     setUsers_per_day(response);
-    console.log("users per day", users_per_day[0]?.USERS_PER_DAY)
-  }
+    console.log("users per day", users_per_day[0]?.USERS_PER_DAY);
+  };
 
-  const get_l_o =async () => {
-    const response = await get_last_online(data.token)
-    setLast_online(response)
-  }
-  const get_l_r =async () => {
-    const response = await get_last_registered(data.token)
-    setLast_registered(response)
-  }
-
-
+  const get_l_o = async () => {
+    const response = await get_last_online(data.token);
+    setLast_online(response);
+  };
+  const get_l_r = async () => {
+    const response = await get_last_registered(data.token);
+    setLast_registered(response);
+  };
 
   const ban = async (id: number, username: string, role: string) => {
-    if(role == 'ADMIN'){ console.log('u cant ban this user')}
-    else{
-    await ban_user(id);
-  }
+    if (role == "ADMIN") {
+      console.log("u cant ban this user");
+    } else {
+      await ban_user(id);
+    }
   };
-  const un_ban = async (id: number, username: string, role: string) => {   
-    await unBan_user(id);  
+  const un_ban = async (id: number, username: string, role: string) => {
+    await unBan_user(id);
   };
-
 
   useEffect(() => {
     if (data.role !== "ADMIN" || data_role) {
       navigate("/");
     } else {
       get_users();
-      get_total_count()
-      get_perday_count()
-      get_l_o()
-      get_l_r()
+      get_total_count();
+      get_perday_count();
+      get_l_o();
+      get_l_r();
     }
   }, []);
- 
-//////////////////////////////////
-/*
+
+  //////////////////////////////////
+  /*
 SETUP STATUS 
 
 NEED ADD IN GLOBAL INFO TABLE PER WEEK, PER DAY LAST
@@ -110,58 +124,68 @@ users sort change or add more sorts
 
 
 */
-/////////////////////////////////
+  /////////////////////////////////
   return (
     <>
       {data.role == "ADMIN" ? (
         <>
-          <Flex >
+          <Flex>
             <div>admin panel users</div>
-            <TableContainer width={window.innerWidth/1.5}>
-            <Table>
-                    <Thead>
-                      <Tr>
-                        <Th >id</Th>
-                        <Th>username</Th>
-                        <Th>Email</Th>
-                        <Th>Role</Th>
-                        <Th>Status</Th>
-                        <Th>Ban</Th>
-                      </Tr>
-                    </Thead><Tbody>
-              {users.map((user) => (
-                
+            <TableContainer width={window.innerWidth / 1.5}>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th>id</Th>
+                    <Th>username</Th>
+                    <Th>Email</Th>
+                    <Th>Role</Th>
+                    <Th>Status</Th>
+                    <Th>Ban</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {users.map((user) => (
                     <Tr key={user.id}>
-                        <Th>{user.id}</Th>
-                        <Th>{user.username}</Th>
-                        <Th>{user.email}</Th>
-                        <Th>{user.role}</Th>
-                        <Th> {user.status}</Th>
-                        <Th>
-                          {user.role == 'ADMIN'? "" :
+                      <Th>{user.id}</Th>
+                      <Th>{user.username}</Th>
+                      <Th>{user.email}</Th>
+                      <Th>{user.role}</Th>
+                      <Th> {user.status}</Th>
+                      <Th>
+                        {user.role === "ADMIN" ? (
+                          ""
+                        ) : user.status === "BANNED" ? (
                           <Button
-                        onClick={() => {
-                          ban(user.id, user.username, user.role);
-                        }}
-                        background={"red"}>
-                          
-                        <Icon as={LuBan} />
-                      </Button>
-                      }
-                      </Th> 
-                     </Tr>
-                 ))}
-              </Tbody>
+                            onClick={() => {
+                              un_ban(user.id, user.username, user.role);
+                            }}
+                            background={'green'}
+                          >
+                            <Icon as={PiCaretDoubleUpBold}  />
+                          </Button>
+                        ) : (
+                          <Button
+                            onClick={() => {
+                              ban(user.id, user.username, user.role);
+                            }}
+                            background={"red"}
+                          >
+                            <Icon as={LuBan} />
+                          </Button>
+                        )}
+                      </Th>
+                    </Tr>
+                  ))}
+                </Tbody>
               </Table>
             </TableContainer>
-            
           </Flex>
           <InputGroup>
-              <InputLeftElement pointerEvents="none">
-                <Icon as={IoIosSearch} color="gray.300" />
-              </InputLeftElement>
-              <Input htmlSize={15} width="auto" placeholder="search user" />
-            </InputGroup>
+            <InputLeftElement pointerEvents="none">
+              <Icon as={IoIosSearch} color="gray.300" />
+            </InputLeftElement>
+            <Input htmlSize={15} width="auto" placeholder="search user" />
+          </InputGroup>
           <StatGroup>
             <Stat>
               <StatLabel>Users total</StatLabel>
