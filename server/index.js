6a953a7c36e8829e874da89225b24eed63e1d805
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2023-09-22 21:08:10 +0300

    wss rooms ***

--- server/index.js
+++ server/index.js
@@ -60,7 +60,76 @@ const brodcastMessage = (message)=>{
     client.send(JSON.stringify(message))
   })
 }
+ /*
+ const setted_rooms = [];
+wss.on("connection", (ws) => {
+  console.log("WebSocket client connected");
+
+  // Handle incoming messages from WebSocket setted_rooms if needed
+  ws.on("message",  (message) => { 
+    const { room } = JSON.parse(message);
+    ///broadcast(message, room)
+    const data = message;
+    console.log(setted_rooms)
+    if (room) {
+      const existingRoom = setted_rooms.find((room_d) => room_d.id === room);
+
+      if (existingRoom) {
+        // If the room exists, add the client to the existing room
+        existingRoom.clients.add(ws);
+        console.log(`Client joined existing room: ${room}`);
+      } else {
+        // If the room doesn't exist, create a new room and add the client to it
+        const newRoom = {
+          id: room,
+          clients: new Set(),
+        };
+        newRoom.clients.add(ws);
+        setted_rooms.push(newRoom);
+        
+        console.log(`Client created and joined new room: ${room}`);
+      }
+ ////-------------------------------------------------------------------------------
+      for (const room_d of setted_rooms) {        
+        if (room_d.clients.has(ws)) {
+          console.log('ws find room')
+          room_d.clients.forEach((client) => {
+            const msg = data.toString()
+            mongo.new_message(msg)
+              client.send(msg);                     
+              console.log("sent to another")
+                    
+            });
+          break; // Stop broadcasting to other setted_rooms
+        }
+      }
+
+
+    } else {
+      // Broadcast the message to the appropriate room_d(s)
+      for (const room_d of setted_rooms) {
+        broadcast(message, room)
+        if (room_d.clients.has(ws)) {
+          console.log('ws find room')
+          room_d.clients.forEach((client) => {
+            client.send(message);
+            
+            console.log("sent to another")
+            console.log(room_d, client)
+          });
+          break; // Stop broadcasting to other setted_rooms
+        }
+      }
+    }
+
+    console.log(`Received message from client: ${data}`);
+    ////  {"action":"joinRoom_d","room":"64f9ce04ba8e3b836ba02186"}
+    ////{"message":"sdfg","from":"64f8d33b9b1734eac643473f","to":"64f8d41c9b1734eac6434740","room_d":"64f9ce04ba8e3b836ba02186"}
+  });
+});
  
+ 
+ */
 
 httpServer.on("upgrade", (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, (ws) => {
