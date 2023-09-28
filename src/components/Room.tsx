24a7d1bdda710aac3ca65fd12da75f3467ba088f
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2023-09-28 20:32:44 +0300

    room component

---
+++ src/components/Room.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Room = (props: any) => {
+
+
+    if(!props){
+        return(
+            <div>
+                here will be default room
+
+            </div>
+        )
+    }
+
+
+
+
+
+    return (
+        <div>
+            main room
+        </div>
+    );
+};
+
+export default Room;
