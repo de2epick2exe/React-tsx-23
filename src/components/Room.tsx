7e997975cc77b076d4484d5f8ac03667de4c8bfe
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2023-09-29 13:25:08 +0300

    default empty room

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -5,11 +5,19 @@ const Room = (props: any) => {
 
     if(!props){
         return(
-            <div>
-                here will be default room
-
+            <div
+            style={{display: "flex",
+            justifyContent: "center", // Center horizontally
+            alignItems: "center",  
+              width: '100%',
+            height: "80%", marginLeft: '20vw'     }}
+            >
+            <p style={{backgroundColor:"red", borderRadius: '10px', padding:"3px", color: 'black'}}> choose someone who you want to sent message </p>
+            
+             
+              
             </div>
-        )
+          );
     }
 
 
