fecd3f36282845d31a86a332b381f91a87115eb6
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2023-10-01 18:38:24 +0300

    messager state

--- src/store/reduses/MessagerSlice.ts
+++ src/store/reduses/MessagerSlice.ts
@@ -1,1 +1,10 @@
 import { createSlice, PayloadAction, ThunkDispatch } from "@reduxjs/toolkit";
+/* db`s tabs:
+|users| <= | conversations| <= |rooms| <= |messages|
+|id   |    |uid    room_id|    | id  |    | room_id|  
+*/
+interface MessagerState {    
+    selected_room: any;
+    rooms: any;
+    room_error: string| null;
+  }
