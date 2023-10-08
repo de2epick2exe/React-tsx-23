a318a61e67133bee88fcd8ae32289e4ca5fbba8b
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2023-10-08 13:11:01 +0300

    notify routes

--- server/routes/messager_routes.js
+++ server/routes/messager_routes.js
@@ -4,6 +4,8 @@ const router = new Router()
 
 router.post('/g/:roomid', Messager.get_room)
 router.post('/new_private_room', Messager.create_private_room)
+router.post('/notify', Messager.target_user_notify)
+router.post('/notify_all', Messager.global_notify)
 
 
 module.exports = router
