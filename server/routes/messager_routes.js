b9fa9192f6934e4a82c6f4df366ea3021a81c194
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2023-10-09 12:41:02 +0300

    routes

--- server/routes/messager_routes.js
+++ server/routes/messager_routes.js
@@ -1,11 +1,13 @@
 const Router = require('express')
 const Messager = require('../controller/Messager')
 const router = new Router()
-
+/// all of it must be in ws
 router.post('/g/:roomid', Messager.get_room)
 router.post('/new_private_room', Messager.create_private_room)
 router.post('/notify', Messager.target_user_notify)
 router.post('/notify_all', Messager.global_notify)
+router.post('/rmsg', Messager.rooms_messages)
+router.post('/msg', Messager.send_message)
 
 
 module.exports = router
