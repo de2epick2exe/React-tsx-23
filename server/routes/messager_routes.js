96a6fde92bb967aee49862e682c8272c0a6a5107
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2023-10-21 14:17:54 +0300

    get notify api

--- server/routes/messager_routes.js
+++ server/routes/messager_routes.js
@@ -7,6 +7,7 @@ router.post('/users_rooms', Messager.get_rooms_list)
 router.post('/new_private_room', Messager.create_private_room)
 router.post('/notify', Messager.target_user_notify)
 router.post('/notify_all', Messager.global_notify)
+router.post('/get_notify',Messager.get_notifies)
 router.post('/rmsg', Messager.rooms_messages)
 router.post('/msg', Messager.send_message)
 
--- src/unite/Messager_functions.tsx
+++ src/unite/Messager_functions.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3033/requests',
+    baseURL: 'http://localhost:3033/room',
   });
 export const get_notify=async (id:number)=>{
- const responce = await api.post('user/login', {id})
+ const responce = await api.post('/get_notify', {id})
  return responce.data
 }
