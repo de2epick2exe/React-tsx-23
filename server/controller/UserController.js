3e6ceeec13f1b1b69b5a38293ef910a07571536e
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2024-01-13 13:53:14 +0200

    delete friend func

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -425,6 +425,10 @@ async accept_friend(id, accepted_id){
   const res = await db.query('UPDATE public.friends SET waiting_accept = array_remove(waiting_accept, $2), friends_list = friends_list || $2 WHERE id = $1',[id, accepted_id])
   return res.rows
 }
+async delete_friend(id, todelete_id){
+  const res = await db.query('UPDATE public.friends SET friends_list = array_remove(friends_list, $2), waiting_accept = waiting_accept || $2 WHERE id = $1',[id, todelete_id])
+  return res.rows
+}
 
 
 
