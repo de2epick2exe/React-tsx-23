7a0d47fd15d247e186c6a71d68f55ad06d919b41
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2024-02-09 11:39:23 +0200

    is login check for add to friends

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,8 +2,23 @@ import React, { useEffect, useRef, useState } from "react";
 import { RootState } from "../store/store";
 import { useDispatch, useSelector } from "react-redux";
 import { ThunkDispatch } from "@reduxjs/toolkit";
-import { Avatar, Box, Button, Center, Flex, Text } from "@chakra-ui/react";
-import { useParams } from "react-router-dom";
+import {
+  Avatar,
+  Box,
+  Button,
+  Center,
+  Flex,
+  Text,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { Link, useParams } from "react-router-dom";
 import { user_profile } from "../unite/User_Functions";
 
 const Profile = () => {
@@ -11,7 +26,12 @@ const Profile = () => {
   const dispatch: ThunkDispatch<any, any, any> = useDispatch();
   const { id } = useParams<{ id?: string }>();
   const [profile_data, Set_Profile_data] = useState<any>();
-  const [is_friend_status, set_Is_friend_status]=useState<any>()
+  const [is_friend_status, set_Is_friend_status] = useState<any>();
+  const {
+    isOpen: isOpenLogin,
+    onOpen: onOpenLogin,
+    onClose: onCloseLogin,
+  } = useDisclosure();
   const get_profile = async () => {
     if (id) {
       const res = await user_profile(parseFloat(id));
@@ -26,23 +46,21 @@ const Profile = () => {
   useEffect(() => {
     socket.current = new WebSocket("ws://localhost:3033");
     socket.current.onopen = () => {
-      console.log(data.username, "connected to ws");         
-      
-
-    };    
+      console.log(data.username, "connected to ws");
+    };
     socket.current.onmessage = (msg) => {
       try {
-        const message = JSON.parse(msg.data);            
-        console.log(message); // for in    
-        switch (message.event) {                           
+        const message = JSON.parse(msg.data);
+        console.log(message); // for in
+        switch (message.event) {
           case "check_state":
             //check if user in hidden list
-            console.log('RETURNS friends lis')                                      
-          break;
+            console.log("RETURNS friends lis");
+            break;
           case "check_user_status":
-              console.log("user status",message.data[0].matched_column );          
-              set_Is_friend_status(message.data[0].matched_column)
-          break;      
+            console.log("user status", message.data[0].matched_column);
+            set_Is_friend_status(message.data[0].matched_column);
+            break;
           default:
             break;
         }
@@ -50,7 +68,6 @@ const Profile = () => {
         console.error("Error parsing JSON:", error);
         console.warn(msg.data);
         console.log(msg.data);
-
       }
     };
     socket.current.onclose = () => {
@@ -59,32 +76,33 @@ const Profile = () => {
     socket.current.onerror = () => {
       console.log("SOCKET error");
     };
-    
+
     return () => {
       if (socket.current) {
         socket.current.close();
       }
-    };  
-   }, []);
+    };
+  }, []);
 
-    const delete_friend = async()=>{
-      if (socket.current && socket.current.readyState === WebSocket.OPEN) {
-        const message = {
-          id: data.id,
-          to_delete: profile_data?.id,
-          event: "delete_friend",
-        };
-        socket.current.send(JSON.stringify(message));
-      } else {
-        // Wait for the connection to open and then send the message
-        setTimeout(() => {
-          delete_friend();
-        }, 100); // You can adjust the timeout value if needed
-      }
+  const delete_friend = async () => {
+    if (socket.current && socket.current.readyState === WebSocket.OPEN) {
+      const message = {
+        id: data.id,
+        to_delete: profile_data?.id,
+        event: "delete_friend",
+      };
+      socket.current.send(JSON.stringify(message));
+    } else {
+      // Wait for the connection to open and then send the message
+      setTimeout(() => {
+        delete_friend();
+      }, 100); // You can adjust the timeout value if needed
     }
-    
-    const add_friend = async()=>{
-      //add check if user authed
+  };
+
+  const add_friend = async () => {
+    //add check if user authed
+    if (data.is_auth) {
       if (socket.current && socket.current.readyState === WebSocket.OPEN) {
         const message = {
           id: data.id,
@@ -93,81 +111,120 @@ const Profile = () => {
         };
         socket.current.send(JSON.stringify(message));
       }
+    } else {
+      onOpenLogin();
     }
-   
-    useEffect(()=>{
-      if (socket.current && socket.current.readyState === WebSocket.OPEN) {
-        if(data.is_auth && id){
-          socket.current.send((JSON.stringify({event: 'check_status', id: data.id, cid:parseFloat(id)  })))
-      }    
+  };
+
+  useEffect(() => {
+    if (socket.current && socket.current.readyState === WebSocket.OPEN) {
+      if (data.is_auth && id) {
+        socket.current.send(
+          JSON.stringify({
+            event: "check_status",
+            id: data.id,
+            cid: parseFloat(id),
+          })
+        );
+      }
     }
-    
-    },[profile_data])
+  }, [profile_data]);
 
-    useEffect(()=>{
-      console.log('USER STATUS IS')
-      console.log('USER STATUS IS')
-      console.log('USER STATUS IS')
-      console.log('USER STATUS IS')
-      console.log('USER STATUS IS')
-      console.log(is_friend_status)
-      console.log('USER STATUS IS')
-      console.log('USER STATUS IS')
-      console.log('USER STATUS IS')
-      console.log('USER STATUS IS')
-     
-    },[is_friend_status])
-    const Friend_button = ()=>{
+  useEffect(() => {
+    console.log("USER STATUS IS");
+    console.log("USER STATUS IS");
+    console.log("USER STATUS IS");
+    console.log("USER STATUS IS");
+    console.log("USER STATUS IS");
+    console.log(is_friend_status);
+    console.log("USER STATUS IS");
+    console.log("USER STATUS IS");
+    console.log("USER STATUS IS");
+    console.log("USER STATUS IS");
+  }, [is_friend_status]);
+  const Friend_button = () => {
+    if (is_friend_status == "friends_list") {
+      return null;
+    }
+    if (is_friend_status == "waiting_accept" || "hidden_not_accepted") {
+      return (
+        <Button mt="2" onClick={add_friend}>
+          Add Friend
+        </Button>
+      );
+    } else {
+      return (
+        <Button mt="2" onClick={add_friend}>
+          Add Friend
+        </Button>
+      );
+    }
 
-      if(is_friend_status == 'friends_list'){
-        return null
-      }
-      if(is_friend_status == 'waiting_accept' ||'hidden_not_accepted'){
-        
-        return (<Button mt='2' onClick={add_friend}>Add Friend</Button>)
-      }
-      else{
-        return(<Button mt='2' onClick={add_friend}>Add Friend</Button>)
-      }
-
-      return null
-      /*
+    return null;
+    /*
       matched_column
       { matched_column: 'friends_list' }
       */
-
-    }
-
-
+  };
 
   return (
     <Box>
       {data.id === (id ? parseFloat(id) : null) ? (
         <Box>
-          <Box m='5' border="2px solid red" width='13vw' padding='3'>
-          
-          <Center>
-           <Avatar size='2xl' name={data.username?? undefined} src={data.photo}/>
-           </Center>
-         
-          <Flex flexDir='column' >
-          <Text as='b'>{data.username}</Text>
-            <Text as='kbd'>{data.role}</Text>
-            </Flex >
+          <Box m="5" border="2px solid red" width="13vw" padding="3">
+            <Center>
+              <Avatar
+                size="2xl"
+                name={data.username ?? undefined}
+                src={data.photo}
+              />
+            </Center>
+
+            <Flex flexDir="column">
+              <Text as="b">{data.username}</Text>
+              <Text as="kbd">{data.role}</Text>
+            </Flex>
           </Box>
         </Box>
       ) : (
         <Box>
-            <Box m='5'  border="2px solid red" width='13vw' padding='3'>
-            <Center><Avatar size='2xl' name={profile_data?.username} src={profile_data?.avatar != null? `http://localhost:8080/img/${profile_data?.avatar}`: `http://localhost:8080/img/default.jpg`}/></Center><br/>
-            <Flex flexDir='column' >
-            <Text as='b'>{profile_data?.username}</Text>
-            <Text as='kbd'>{profile_data?.role}</Text>
-            
-            <Button mt='2'>Write message</Button>
-           <Friend_button/>
+          <Box m="5" border="2px solid red" width="13vw" padding="3">
+            <Center>
+              <Avatar
+                size="2xl"
+                name={profile_data?.username}
+                src={
+                  profile_data?.avatar != null
+                    ? `http://localhost:8080/img/${profile_data?.avatar}`
+                    : `http://localhost:8080/img/default.jpg`
+                }
+              />
+            </Center>
+            <br />
+            <Flex flexDir="column">
+              <Text as="b">{profile_data?.username}</Text>
+              <Text as="kbd">{profile_data?.role}</Text>
+              <Button mt="2">Write message</Button>
+              <Friend_button />
             </Flex>
-            </Box>
+            <Modal isOpen={isOpenLogin} onClose={onCloseLogin}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>You mus be logined</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                <Button variant="ghost" as={Link} to="/login">Login</Button>
+                </ModalBody>
+
+                <ModalFooter>
+                  <Button colorScheme="blue" mr={3} onClick={onCloseLogin}>
+                    Close
+                  </Button>
+                  
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          </Box>
         </Box>
       )}
     </Box>
