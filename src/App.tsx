d1dc49f13cd4c665bc2ce38d847881000e14d1f8
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2024-02-29 14:57:13 +0200

    styling/cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "./App.css";
 import { useSelector } from "react-redux";
 import { RootState } from "./store/store";
+import { Box, Flex } from "@chakra-ui/react";
 
 function App() {
   const data = useSelector((state: RootState) => state.userReducer);
@@ -10,12 +11,19 @@ function App() {
       <div className="App page" >
         
         {
-          data.is_auth ? <><p>user authed </p>
+          data.is_auth ? 
+          <>
+          <Flex justifyContent={'space-between'}>
+          <Box><p>user authed </p>
           <p >Home page user : {" " + data.username + " "} </p> 
         {" " + data.email + " "}
         {" " + data.username + " "}
-        
-          
+        </Box><Box>
+          <Box border='1px solid grey'>
+             statistick
+             </Box>
+          <Box> some streams</Box></Box>
+          </Flex>
           
           </>
           : 
