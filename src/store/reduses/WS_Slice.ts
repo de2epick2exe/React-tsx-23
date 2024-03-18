3d040cd354b5b2277eb95c01ea5fff9c99357f9d
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2024-03-18 11:27:57 +0200

    ws initial state

--- src/store/reduses/WS_Slice.ts
+++ src/store/reduses/WS_Slice.ts
@@ -6,13 +6,33 @@ interface Channel{
     followers: number;
 }
 interface Notify{
-    
+    from: string
+    message: string
+    avatar: string
 }
 
 interface WS{
     socket: WebSocket | null;
     message: any;
     messages: [];
+    notifies : Notify[]
     current_channel: Channel|null;
     connected:boolean;    
 }
+
+const initialState: WS = {
+    socket: null,
+    message: "",
+    messages: [],
+    notifies:[],
+    current_channel: null,
+    connected:false,
+  };
+
+export const WSSlice = createSlice({
+    name: 'WS',
+    initialState,
+    reducers:{
+
+    }
+})
