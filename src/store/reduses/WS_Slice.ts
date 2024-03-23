2c2a62e69c2e0bb5498016d36b318cd009553534
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2024-03-23 17:17:18 +0200

    posts ws front

--- src/store/reduses/WS_Slice.ts
+++ src/store/reduses/WS_Slice.ts
@@ -1,107 +1,127 @@
 import { createSlice, PayloadAction, ThunkDispatch } from "@reduxjs/toolkit";
 import { RootState } from "../store";
-interface Channel{
-    id:number;
-    channel_name: string;
-    status:boolean;    
-    followers: number;
+interface Channel {
+  id: number;
+  channel_name: string;
+  status: boolean;
+  followers: number;
 }
-interface Notify{
-    from: string
-    message: string
-    avatar: string
+interface Notify {
+  from: string;
+  message: string;
+  avatar: string;
 }
-interface Message{
-    title: string;
-    text: string | boolean;
-    media: string | boolean;
-    emotes: []
+interface Message {
+  title: string;
+  text: string | boolean;
+  media: string | boolean;
+  emotes: [];
 }
-interface WS{
-    socket: WebSocket | null;
-    message: any;
-    messages: Message[];
-    notifies : Notify[]
-    current_channel: Channel|null;
-    connected:boolean;    
+interface Post {
+  title: string;
+  content: string;
+  media: any;
+  emotes: [];
+}
+interface WS {
+  socket: WebSocket | null;
+  message: any;
+  messages: Message[];
+  notifies: Notify[];
+  posts: Post[];
+  current_channel: Channel | null;
+  connected: boolean;
 }
 
 const initialState: WS = {
-    socket: null,
-    message: "",
-    messages: [],
-    notifies:[],
-    current_channel: null,
-    connected:false,
-  };
+  socket: null,
+  message: "",
+  messages: [],
+  notifies: [],
+  posts: [],
+  current_channel: null,
+  connected: false,
+};
 
 export const WS_Slice = createSlice({
-    name: 'WS',
-    initialState,
-    reducers:{
-        setSocket:(state, action: PayloadAction< WebSocket | null>)=>{
-            state.socket = action.payload;            
-          },
-          setMessage: (state, action: PayloadAction<string | null>) => {
-            state.message = action.payload;
-          },
-          setMessages: (state, action: PayloadAction<Message[] | null>) => {
-            state.messages = action.payload!;  // check if ?? works          
-          },
-          setNotifies:(state, action: PayloadAction<Notify[] | null>) => {
-            state.notifies = action.payload!
-          },
-          setChannel: (state, action: PayloadAction<Channel | null>) => {
-            state.current_channel = action.payload;            
-          },
-          setConnected: (state, action: PayloadAction<boolean>) => {
-            state.connected = action.payload;            
-          }
-    }
-})
-export const { setSocket, setMessage, setMessages, setChannel,setConnected, setNotifies} =
-  WS_Slice.actions;
+  name: "WS",
+  initialState,
+  reducers: {
+    setSocket: (state, action: PayloadAction<WebSocket | null>) => {
+      state.socket = action.payload;
+    },
+    setMessage: (state, action: PayloadAction<string | null>) => {
+      state.message = action.payload;
+    },
+    setMessages: (state, action: PayloadAction<Message[] | null>) => {
+      state.messages = action.payload!; // check if ?? works
+    },
+    setNotifies: (state, action: PayloadAction<Notify[] | null>) => {
+      state.notifies = action.payload!;
+    },
+    setPosts: (state, action: PayloadAction<Post[] | null>) => {
+      state.posts = action.payload!;
+    },
+    setChannel: (state, action: PayloadAction<Channel | null>) => {
+      state.current_channel = action.payload;
+    },
+    setConnected: (state, action: PayloadAction<boolean>) => {
+      state.connected = action.payload;
+    },
+  },
+});
+export const {
+  setSocket,
+  setMessage,
+  setMessages,
+  setChannel,
+  setConnected,
+  setNotifies,
+  setPosts,
+} = WS_Slice.actions;
 
-  export const connectToWebSocket = () => {
-    return (dispatch: any) => {
-        const socket = new WebSocket("ws://localhost:8080"); // Adjust URL accordingly
+export const connectToWebSocket = () => {
+  return (dispatch: any) => {
+    const socket = new WebSocket("ws://localhost:8080"); // Adjust URL accordingly
 
-        socket.onopen = () => {
-            dispatch(setConnected(true));
-        };
-
-        socket.onmessage = (event) => {
-            const message= JSON.parse(event.data);
-             switch (message.event) {
-              case 'Notify':
-                dispatch(setNotifies(message.notify))
-                break;
-             
-              default:
-                break;
-             }       
-        };
-
-        socket.onerror = (error) => {
-            console.error("WebSocket error:", error);
-        };
-
-        socket.onclose = () => {
-            dispatch(setConnected(false));
-        };
-
-        dispatch(setSocket(socket));
+    socket.onopen = () => {
+      dispatch(setConnected(true));
     };
-};  
+
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      switch (message.event) {
+        case "notify":
+          dispatch(setNotifies(message.notify));
+          break;
+        case "get_posts":
+          dispatch(setPosts(message.posts));
+          break;
+        default:
+          break;
+      }
+    };
+
+    socket.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+
+    socket.onclose = () => {
+      dispatch(setConnected(false));
+    };
+
+    dispatch(setSocket(socket));
+  };
+};
 
 export const sendMessage = (message: any) => {
   return (dispatch: ThunkDispatch<{}, {}, any>, getState: () => RootState) => {
-      const { socket } = getState().WS_Slice;
-      if (socket && socket.readyState === WebSocket.OPEN) {
-          socket.send(JSON.stringify(message));
-      } else {
-          console.error("WebSocket connection is not open.");
-      }
+    const { socket } = getState().WS_Slice;
+    if (socket && socket.readyState === WebSocket.OPEN) {
+      socket.send(JSON.stringify(message));
+    } else {
+      console.error("WebSocket connection is not open.");
+    }
   };
 };
 
