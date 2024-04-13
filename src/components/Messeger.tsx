29e140573a9cd0278fd338b183e178dfbc10da2c
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2024-04-13 15:04:11 +0300

    changes

--- src/components/Messeger.tsx
+++ src/components/Messeger.tsx
@@ -7,6 +7,13 @@ import {
   InputGroup,
   position,
   Box,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  Portal,
+  PopoverBody,
+  Text,
 } from "@chakra-ui/react";
 import { useSelector, useDispatch } from "react-redux";
 import { ThunkDispatch } from "@reduxjs/toolkit";
@@ -18,6 +25,8 @@ import {
   setMessages,
 } from "../store/reduses/MessagerSlice";
 import { sendMessage } from "../store/reduses/WS_Slice";
+import { EditIcon, CloseIcon } from "@chakra-ui/icons";
+import { AiFillNotification, AiOutlineTeam, AiOutlineUser } from "react-icons/ai";
 
 interface room_user {
   id: number;
@@ -33,7 +42,7 @@ const Messenger = () => {
   const [room, setRoomState] = useState<number | undefined>();
   const [selected_room, setSelected_room] = useState("");
   const [message_State, setMesage_state] = useState(false);
- 
+
   const data = useSelector((state: RootState) => state.userReducer);
   const messager = useSelector((state: RootState) => state.messagerReducer);
   const ws = useSelector((state: RootState) => state.WS_Slice);
@@ -41,7 +50,7 @@ const Messenger = () => {
   const messagesEndRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    setRooms(messager.rooms)
+    setRooms(messager.rooms);
   }, [messager.rooms]);
 
   useEffect(() => {
@@ -65,14 +74,14 @@ const Messenger = () => {
       room: id,
       event: "connection_to_room",
     };
-    
+
     dispatch(sendMessage(connection_to_room));
 
     const message = {
       room_id: id,
       event: "rooms_messages",
     };
-    
+
     dispatch(sendMessage(message));
   };
 
@@ -117,7 +126,6 @@ const Messenger = () => {
     );
   };
 
-  
   return (
     <>
       <Grid
@@ -134,24 +142,56 @@ const Messenger = () => {
           margin: "0",
         }}
       >
-        <GridItem pl="2" bg="red" area={"all-chats"}>
+        
+        <GridItem pl="2" bg="red" area={"all-chats"} position="relative">
+        <GridItem >
+          folders
+        </GridItem>
           All Chats
           {rooms?.map((r) => (
-          <span key={r.id}>
-            <br />
-            <Button
-              
-              bg="black"
-              ml="-1"
-              width="100% "
-              variant="ghost"
-              onClick={(e) => setRoomdata(r)}
-            >
-              {r.username}
-            </Button>
-          </span>
-        ))}
-        
+            <span key={r.id}>
+              <br />
+              <Button
+                bg="black"
+                ml="-1"
+                width="100% "
+                variant="ghost"
+                onClick={(e) => setRoomdata(r)}
+              >
+                {r.username}
+              </Button>
+            </span>
+          ))}
+          <Box>
+            <Popover>
+              <PopoverTrigger>
+              <EditIcon
+              w="12"
+              h="12"
+              cursor="pointer"
+              borderRadius="full"
+              p="2"
+              position="absolute"
+              bg="blackAlpha.900"
+              bottom="2"
+              right="2"
+              textColor="white"
+            />
+              </PopoverTrigger>
+              <Portal >
+                <PopoverContent w='6vw' >   
+                  <PopoverBody display='flex'  flexDirection='column' p='0' >
+                    <Button borderRadius='none' p='0' m='0' display='flex' justifyContent='start'><AiFillNotification fontSize='md'  /><Text fontSize='xs'>New channel</Text></Button>
+                    <Button borderRadius='none' p='0' m='0' display='flex' justifyContent='start'><AiOutlineTeam fontSize='md' /> <Text fontSize='xs' >New group</Text></Button>
+                    <Button borderRadius='none' p='0' m='0' display='flex' justifyContent='start'> <AiOutlineUser fontSize='md'  /><Text fontSize='xs' >New private</Text> </Button>
+
+                  </PopoverBody>                  
+                </PopoverContent>
+              </Portal>
+            </Popover>
+
+       
+          </Box>
         </GridItem>
 
         <GridItem bg="black" area={"chat"}>
