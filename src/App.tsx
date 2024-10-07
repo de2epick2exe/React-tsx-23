72bc914776cd26f840e4ad04be238683ed1fb47c
Epick <42617421+DenisKrivosheev@users.noreply.github.com>
2024-10-07 11:17:12 +0300

    Feed component && changes

--- src/App.tsx
+++ src/App.tsx
@@ -21,75 +21,11 @@ function App() {
       console.log("app tsx ws error");
     }
   }, [ws.connected]);
-  const checkws = () => {
-    console.log('checkws')
-    dispatch(sendMessage({ event: "get_notify", id: data.id }))
-  };
-
-  const Feed_Component = ()=>{
-
-
-    return messager.recomends.map((post, index)=>(
-    <>
-    <Box
-        key={index}
-        border={"2px solid black"}
-        my="5"
-        px="2"
-        borderRadius="7px"
-      >
-        <Flex mt="1" justifyContent="space-between">
-          <Flex>           
-            username
-          </Flex>          
-            <Menu>
-              <MenuButton
-                as={IconButton}
-                aria-label="Options"
-                icon={<HamburgerIcon />}
-                variant="outline"
-              />
-              <MenuList>
-                <MenuItem
-                  icon={<EditIcon />}
-                  
-                >
-                  Edit
-                </MenuItem>
-                <MenuItem icon={<DeleteIcon />} >Delete</MenuItem>
-              </MenuList>
-            </Menu>
-        </Flex></Box>       
-    </>
-    ))}
-
-
   return (
     <>
       <div className="App page">
         {data.is_auth ? (
-          <>
-            <Grid templateColumns="repeat(3, 1fr)" gap='1' h='92vh'>
-              <GridItem>
-              <Box>
-                <p>user authed </p>
-                <p>Home page user : {" " + data.username + " "} </p>
-                {" " + data.email + " "}
-                {" " + data.username + " "}
-                <Button onClick={checkws}>check</Button>
-              </Box>
-              </GridItem>
-              <GridItem borderX='2px solid red'>
-                Feed
-              </GridItem>
-              <GridItem>
-              <Box>
-                <Box border="1px solid grey">statistick</Box>
-                <Box> some streams</Box>
-              </Box>
-              </GridItem>
-              </Grid>
-          </>
+                   ''
         ) : (
           "user not auth"
         )}
---
+++ src/components/Feed.tsx
@@ -0,0 +1,101 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store/store";
+import { Box, Button, Flex, Grid, GridItem, IconButton, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { sendMessage } from "../store/reduses/WS_Slice";
+import { ThunkDispatch } from "@reduxjs/toolkit";
+import { DeleteIcon, EditIcon, HamburgerIcon } from "@chakra-ui/icons";
+
+function Feed() {
+  const data = useSelector((state: RootState) => state.userReducer);
+  const ws = useSelector((state: RootState) => state.WS_Slice);
+  const messager = useSelector((state: RootState) => state.messagerReducer);
+
+  const dispatch: ThunkDispatch<any, any, any> = useDispatch();
+  useEffect(() => {
+    try {
+      console.log("WEBSOCKET MAIN STATE IS:", ws.connected);
+      dispatch(sendMessage({ event: "get_notify", id: data.id }))
+    } catch (error) {
+      console.log("app tsx ws error");
+    }
+  }, [ws.connected]);
+  const checkws = () => {
+    console.log('checkws')
+    dispatch(sendMessage({ event: "get_notify", id: data.id }))
+  };
+
+  const Feed_Component = ()=>{
+
+
+    return messager.recomends.map((post, index)=>(
+    <>
+    <Box
+        key={index}
+        border={"2px solid black"}
+        my="5"
+        px="2"
+        borderRadius="7px"
+      >
+        <Flex mt="1" justifyContent="space-between">
+          <Flex>           
+            username
+          </Flex>          
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label="Options"
+                icon={<HamburgerIcon />}
+                variant="outline"
+              />
+              <MenuList>
+                <MenuItem
+                  icon={<EditIcon />}
+                  
+                >
+                  Edit
+                </MenuItem>
+                <MenuItem icon={<DeleteIcon />} >Delete</MenuItem>
+              </MenuList>
+            </Menu>
+        </Flex></Box>       
+    </>
+    ))}
+
+
+  return (
+    <>
+      <div className="App page">
+        {data.is_auth ? (
+          <>
+            <Grid templateColumns="repeat(3, 1fr)" gap='1' h='92vh'>
+              <GridItem>
+              <Box>
+                <p>user authed </p>
+                <p>Home page user : {" " + data.username + " "} </p>
+                {" " + data.email + " "}
+                {" " + data.username + " "}
+                <Button onClick={checkws}>check</Button>
+              </Box>
+              </GridItem>
+              <GridItem borderX='2px solid red'>
+                Feed
+              </GridItem>
+              <GridItem>
+              <Box>
+                <Box border="1px solid grey">statistick</Box>
+                <Box> some streams</Box>
+              </Box>
+              </GridItem>
+              </Grid>
+          </>
+        ) : (
+          "user not auth"
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Feed;
