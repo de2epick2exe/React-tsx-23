e6d886e33340999759595fa34e8a27f4e6815800
Epick <42617421+de2epick2exe@users.noreply.github.com>
2024-10-31 11:29:51 +0200

    Get post useEffect

--- src/components/Post_page.tsx
+++ src/components/Post_page.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { RootState } from "../store/store";
 import { useDispatch, useSelector } from "react-redux";
 import { ThunkDispatch } from "@reduxjs/toolkit";
 import { Link, useParams } from "react-router-dom";
+import { sendMessage } from '../store/reduses/WS_Slice';
 export const Post_page = () => {
     const data = useSelector((state: RootState) => state.userReducer);
     const ws = useSelector((state: RootState) => state.WS_Slice);
@@ -11,6 +12,10 @@ export const Post_page = () => {
     );
     const dispatch: ThunkDispatch<any, any, any> = useDispatch();
     const { id } = useParams<{ id?: string }>();
+    useEffect(()=>{
+      dispatch(sendMessage({ event: "get_user_post", id: id }))
+    },[ws.connected])
+    
   return (
     <div>Post_page</div>
   )
