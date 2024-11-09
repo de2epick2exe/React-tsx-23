1030254b108f604451e6e73d4794d696e49f58ad
Epick <42617421+de2epick2exe@users.noreply.github.com>
2024-11-09 16:40:54 +0200

    comments map

--- src/components/Post_page.tsx
+++ src/components/Post_page.tsx
@@ -1,10 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { RootState } from "../store/store";
 import { useDispatch, useSelector } from "react-redux";
 import { ThunkDispatch } from "@reduxjs/toolkit";
 import { Link, useParams } from "react-router-dom";
 import { sendMessage } from "../store/reduses/WS_Slice";
-import { Avatar, Box, Divider, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Flex,
+  Grid,
+  GridItem,
+  Text,
+} from "@chakra-ui/react";
 export const Post_page = () => {
   const data = useSelector((state: RootState) => state.userReducer);
   const ws = useSelector((state: RootState) => state.WS_Slice);
@@ -13,33 +21,53 @@ export const Post_page = () => {
   );
   const dispatch: ThunkDispatch<any, any, any> = useDispatch();
   const { id } = useParams<{ id?: string }>();
+  const [comment, setComment] = useState("")
+
+
   useEffect(() => {
     dispatch(sendMessage({ event: "get_user_post", id: id }));
   }, [ws.connected]);
 
+  const send_comment = () => {
+    dispatch(sendMessage({
+      event: "comment",
+      date: new Date(),
+      comment: comment,
+      post_id: id
+    }))
+
+  };
+
   return (
     <>
-      <Grid templateColumns="repeat(3, 1fr)" gap="1" h="92vh" >
+      <Grid templateColumns="repeat(3, 1fr)" gap="1" h="92vh">
         <GridItem></GridItem>
-        <Grid borderX={'1px solid red '}   >
-          <Flex bg={'black'} mx={'5'} flexDir={"column"} >
-            <Flex my={'2'}>
-            <Avatar mx={'5'}
-              size="sm"
-              name={messager_store.current_userPost?.username} 
-              src={
-                messager_store.current_userPost?.avatar != null
-                  ? `http://localhost:8080/img/${messager_store.current_userPost?.avatar}`
-                  : `http://localhost:8080/img/default.jpg`
-              }
-            />
-            <Flex flexDir={"column"}>
-              <Text ml="1">{messager_store.current_userPost?.username}</Text>
-              <Text ml="1">{messager_store.current_userPost?.post}</Text>
+        <Grid borderX={"1px solid red "}>
+          <Flex bg={"black"} mx={"5"} flexDir={"column"}>
+            <Flex my={"2"}>
+              <Avatar
+                mx={"5"}
+                size="sm"
+                name={messager_store.current_userPost?.username}
+                src={
+                  messager_store.current_userPost?.avatar != null
+                    ? `http://localhost:8080/img/${messager_store.current_userPost?.avatar}`
+                    : `http://localhost:8080/img/default.jpg`
+                }
+              />
+              <Flex flexDir={"column"}>
+                <Text ml="1">{messager_store.current_userPost?.username}</Text>
+                <Text ml="1">{messager_store.current_userPost?.post}</Text>
+              </Flex>
             </Flex>
-            </Flex>
-            <Box border={'1px solid red'}/>
-            
+            <Box border={"1px solid red"} />
+            <Box>
+              {messager_store.current_userPost.comments.map((post) => (
+                <span key={post.id}>
+                  <Text>{post.comment}</Text>
+                </span>
+              ))}
+            </Box>
           </Flex>
         </Grid>
         <Grid></Grid>
